2130bf0b4a0b39856e158bf84b291b350329aca8
SimigiuAndrei <andy_andrei9572@yahoo.com>
2023-03-13 12:24:07 +0200

    static files

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,13 @@ app.get("/api/config/paypal", (req, res) =>
 
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5001;
 app.listen(port, () =>
   console.log(
